29946673209c5317de50ab533f0bd0e2ce1774a5
ghufron akbar <alan.040219.gam@gmail.com>
2024-09-23 21:27:14 +0700

    add

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,9 @@
-import SpinnerLoading from "@/components/ui/SpinnerLoading";
-import { refresh } from "@/services/auth";
 import { useFonts } from "expo-font";
-import { router, Slot, SplashScreen } from "expo-router";
-import { useEffect, useState } from "react";
+import { Slot, SplashScreen } from "expo-router";
+import { useEffect } from "react";
 import { View } from "react-native";
 import Toast from "react-native-toast-message";
-import { LogBox } from 'react-native';
+import { LogBox } from "react-native";
 
 LogBox.ignoreAllLogs();
 SplashScreen.preventAutoHideAsync();
@@ -15,35 +13,13 @@ export default function Layout() {
     Inter: require("../assets/fonts/Inter.ttf"),
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
-  // const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     if (fontsLoaded) {
       SplashScreen.hideAsync();
     }
   }, [fontsLoaded]);
-
-  // useEffect(() => {
-  //   const handleAuthCheck = async () => {
-  //     try {
-  //       await refresh();
-  //       router.push("/(home)");
-  //     } catch (error) {
-  //       Toast.show({
-  //         type: "error",
-  //         text1: "Sesi Kadaluarsa",
-  //         text2: "Silahkan login kembali",
-  //       });
-  //       router.push("/login");
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   if (loading || !fontsLoaded) {
-  //     handleAuthCheck();
-  //   }
-  // }, [loading]);
+  
   return (
     <View style={{ flex: 1 }}>
       <Slot />
